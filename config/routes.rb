80dae09f644d99b74c01af3cf5a505fdb8e0927d
Marie Amelie Sandrock <msandrock@audencia.com>
2018-11-13 17:45:57 +0100

    CRU done, delete left if motivated + styling can be improved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get '/articles', to: 'articles#index', as: :articles
+get '/articles/new', to: 'articles#new', as: :newarticle
+post '/articles', to: 'articles#create'
+get '/articles/:id', to: 'articles#show', as: :article
+get '/articles/:id/edit', to: 'articles#edit', as: :edit
+patch'/articles/:id', to: 'articles#update'
 end
